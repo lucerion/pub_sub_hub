e7b00750d81b257f1903bbda346daa24c7fe1765
lucerion <alexander.skachko@gmail.com>
2020-02-26 18:01:14 +0300

    Change token generation

--- apps/hub/lib/token.ex
+++ apps/hub/lib/token.ex
@@ -3,10 +3,22 @@ defmodule PubSubHub.Hub.Token do
 
   alias PubSubHub.Hub.Repo
 
+  @default_secret_key_length 16
+
+  @doc "Generates random secret key"
+  @spec generate(integer) :: String.t()
+  def generate(length) do
+    length
+    |> :crypto.strong_rand_bytes()
+    |> Base.encode16(case: :lower)
+  end
+
+  def generate, do: generate(@default_secret_key_length)
+
   @spec refresh(struct) :: {:ok, String.t()} | {:error, nil}
   def refresh(entity) do
     entity
-    |> entity.__struct__.update_changeset(%{token: Bcrypt.gen_salt()})
+    |> entity.__struct__.update_changeset(%{token: generate()})
     |> Repo.update()
     |> extract_token()
   end
