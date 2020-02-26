94afd95a8148e178b20ad30db9302ca43779aef8
lucerion <alexander.skachko@gmail.com>
2020-02-26 07:52:44 +0300

    Update seeds

--- apps/hub/priv/repo/seeds.exs
+++ apps/hub/priv/repo/seeds.exs
@@ -1,4 +1,8 @@
-alias PubSubHub.Hub.{Subscribers, Publishers}
+alias PubSubHub.Hub
+alias PubSubHub.Hub.{Publishers, Channels, Subscribers}
 
-{:ok, subscriber } = Subscribers.create(%{email: "subscriber@example.com", secret: "subscriber_secret"})
 {:ok, publisher} = Publishers.create(%{email: "publisher@example.com", secret: "publisher_secret"})
+{:ok, channel} = Channels.create(%{url: "example.com/channel", publisher_id: publisher.id, secret: "channel_secret"})
+
+{:ok, subscriber} = Subscribers.create(%{email: "subscriber@example.com", secret: "subscriber_secret"})
+{:ok, subscription} = Hub.subscribe(subscriber, channel, %{callback_url: "example.com/callback"})
