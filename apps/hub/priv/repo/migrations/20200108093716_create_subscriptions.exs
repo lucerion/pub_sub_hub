fda65c83fc978ef257b1d2c108c265ce1a726350
lucerion <alexander.skachko@gmail.com>
2022-01-13 09:14:05 +0300

    Delete callback_url from subscription

--- apps/hub/priv/repo/migrations/20200108093716_create_subscriptions.exs
+++ apps/hub/priv/repo/migrations/20200108093716_create_subscriptions.exs
@@ -3,7 +3,6 @@ defmodule PubSubHub.Hub.Repo.Migrations.CreateSubscriptions do
 
   def change do
     create table(:subscriptions) do
-      add :callback_url, :string, null: false
       add :user_id, references(:users, on_delete: :delete_all)
       add :channel_id, references(:channels, on_delete: :delete_all)
 
